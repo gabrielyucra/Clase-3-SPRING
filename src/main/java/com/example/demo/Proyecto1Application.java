344649c337cfb2a5ddd41f57c99df750b11acd8e
gabriel yucra <gabriel.yucra_99@hotmail.com>
2023-04-19 02:54:43 -0300

    Initial commit

---
+++ src/main/java/com/example/demo/Proyecto1Application.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Scanner;
+
+@SpringBootApplication
+public class Proyecto1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto1Application.class, args);
+
+		Scanner escaner = new Scanner(System.in);
+		System.out.println("ingresa tu nombre");
+		String name = escaner.nextLine();
+		System.out.println("ggg " + name);
+
+	}
+
+}
